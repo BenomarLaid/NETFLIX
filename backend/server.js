59fc9e289b01ae859626391931508fef6cf94581
BenomarLaid <95710717+BenomarLaid@users.noreply.github.com>
2025-02-09 14:20:32 -0500

    add-search

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,24 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoute from "./routes/tv.route.js";
+import searchRoute from "./routes/search.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //will allow to parse req.body
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoute);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoute);
+app.use("/api/v1/search", protectRoute, searchRoute);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
@@ -24,4 +29,3 @@ app.listen(PORT, () => {
 
 //benomar20142014
 //CSjzhU2FSMvHNZoK
-
