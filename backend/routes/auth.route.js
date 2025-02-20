9d4c577f78a903c3f4f51068fb3806ac6f46eb6b
BenomarLaid <95710717+BenomarLaid@users.noreply.github.com>
2025-02-20 00:00:08 -0500

    #

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import { protectRoute } from "../middleware/protectRoute.js";
 
-import { login, logout, signup } from '../controllers/auth.controller.js';
+
+import { login, logout, signup, authCheck } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/authCheck", protectRoute,authCheck);
+router.get("/authCheck", protectRoute,authCheck);
 
 export default router
