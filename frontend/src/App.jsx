fc3dffeca14ff99136f232e97a635d852b704299
BenomarLaid <95710717+BenomarLaid@users.noreply.github.com>
2025-02-22 21:08:46 -0500

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,9 @@ import { Toaster } from "react-hot-toast"
 import { useAuthStore } from "./store/authUser"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
-import { WatchPage } from "./pages/Home/WatchPage"
-import SearchPage from "./pages/SearchPAge" 
+import { WatchPage } from "./pages/Home/WatchPage";
+import SearchPage from "./pages/SearchPage"
+import SearchHistoryPage from "./pages/SearchHistoryPage"
 
 
 function App() {
@@ -35,8 +36,9 @@ function App() {
 				<Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
 				<Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
 				<Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
-				<Route path='/search/:id' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
-        
+        <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+
 
     </Routes>
     <Footer/>
